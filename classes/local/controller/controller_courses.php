36fbc57c8d1c47cc8f04873b87389facfda78f32
Thomas Dondorf <dondorf@lfi.rwth-aachen.de>
2018-04-16 16:01:10 +0200

    Course overview improved

--- classes/local/controller/controller_courses.php
+++ classes/local/controller/controller_courses.php
@@ -27,10 +27,7 @@
 namespace local_learning_analytics\local\controller;
 
 use local_learning_analytics\controller_base;
-
 use local_learning_analytics\local\outputs\table;
-use context_course;
-use moodle_exception;
 
 class controller_courses extends controller_base {
 
@@ -44,79 +41,60 @@ class controller_courses extends controller_base {
         $table = new table();
 
         $query = <<<SQL
-        SELECT `id`, `fullname`
-        FROM {course}
-        WHERE id IN (
+        SELECT
+            c.id,
+            c.fullname AS course_fullname,
+            cc.name AS category_name,
+            (SELECT COUNT(DISTINCT u.id)
+                FROM {user} u
+                JOIN {user_enrolments} ue
+                    ON ue.userid = u.id
+                JOIN {enrol} e
+                    ON e.id = ue.enrolid
+                    WHERE u.deleted = 0
+                    AND e.courseid = c.id) as students,
+            (SELECT ROUND(AVG(g.finalgrade), 2)
+                FROM {grade_items} gi
+                JOIN {grade_grades} g
+                    ON g.itemid = gi.id
+                    WHERE gi.courseid = c.id
+                    AND gi.itemtype = 'course'
+                    AND g.finalgrade IS NOT NULL) as avg_grade,
+            (SELECT COUNT(*)
+                FROM {course_sections}
+                WHERE visible = 1
+                AND course = c.id) AS sections,
+            (SELECT COUNT(*)
+                FROM {course_modules}
+                WHERE visible = 1
+                AND course = c.id) AS activities
+        FROM {course} c
+        # category
+        JOIN {course_categories} cc
+            ON cc.id = c.category
+            # AND cc.visible = 1 # TODO uncomment this, but for now leave it so we have more data to show
+        # restrict to courses of the lecturer
+        WHERE c.id IN (
             SELECT instanceid FROM {context}
             WHERE id IN (
-                SELECT `contextid`
-                FROM `{role_assignments}`
-                WHERE
-                    userid = ? AND
-                    `roleid` <= 4
+                SELECT contextid
+                FROM {role_assignments}
+                WHERE userid = ? AND roleid <= 4
             ) AND contextlevel = 50
         )
+        GROUP BY c.id
+        ORDER BY cc.sortorder, c.fullname
 SQL;
-        //  TODO: add something like this: AND `startdate` <= UNIX_TIMESTAMP() AND `enddate` > UNIX_TIMESTAMP()
 
-        $table->set_header_local(['course_name', 'category', 'student_count', 'avg_grade', 'sections', 'activities'], 'local_learning_analytics');
+        $table->set_header_local(['course_name', 'category', 'students', 'avg_grade', 'sections', 'activities'], 'local_learning_analytics');
 
         $courses = $DB->get_records_sql($query, [$USER->id]);
 
-        $table->add_rows_ajax(self::class . '@ajax', array_map(function($e) {
-            return ['content' => $e->fullname, 'id' => $e->id];
-        }, $courses));
-
         foreach ($courses as $course) {
-            $table->data[] = [$course->fullname, $course->id];
+            $table->add_row([$course->course_fullname, $course->category_name, $course->students, $course->avg_grade, $course->sections, $course->activities]);
         }
 
         return $table->print();
-
     }
 
-    /**
-     * @param string $id
-     * @return array
-     * @throws \dml_exception
-     * @throws \required_capability_exception
-     * @throws moodle_exception
-     */
-    public function ajax(string $id) {
-        global $DB;
-        $courseid = (int) $id;
-
-        $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
-        $context = context_course::instance($course->id, MUST_EXIST);
-
-        if ($course->id == SITEID) {
-            throw new moodle_exception('invalidcourse');
-        }
-        // only teachers and managers
-        require_capability('moodle/course:update', $context);
-
-        $courseQuery = <<<SQL
-    SELECT
-      cc.name AS category_name,
-    (SELECT COUNT(DISTINCT u.id)
-        FROM {user} u
-            JOIN {user_enrolments} ue ON ue.userid = u.id
-            JOIN {enrol} e ON (e.id = ue.enrolid AND e.courseid = ${courseid})
-            WHERE 1 = 1 AND u.deleted = 0) as student_count,
-    (SELECT ROUND(AVG(g.finalgrade), 2)
-        FROM mdl_grade_items gi
-        JOIN mdl_grade_grades g
-            ON g.itemid = gi.id
-            WHERE gi.itemtype = 'course'
-                AND g.finalgrade IS NOT NULL
-                AND gi.courseid = ${courseid}) as avg_grade,
-    (SELECT COUNT(*) FROM {course_sections} WHERE visible = 1 AND course = ${courseid}) AS sections,
-    (SELECT COUNT(*) FROM {course_modules} WHERE visible = 1 AND course = ${courseid}) AS activities
-    FROM {course} c
-    JOIN {course_categories} cc ON cc.id = c.category
-      WHERE c.id = ${courseid};
-SQL;
-
-        return array_values(json_decode(json_encode(array_values($DB->get_records_sql($courseQuery, []))[0]), true));
-    }
 }
