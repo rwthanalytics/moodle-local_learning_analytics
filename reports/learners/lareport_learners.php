494a5b4b1b3ec138ba1602d7fffb44012ec86c9a
Thomas Dondorf <dondorf@win.lfi.rwth-aachen.de>
2020-03-11 15:31:15 +0100

    Increased number of rows to show by default in learners report to 10

--- reports/learners/lareport_learners.php
+++ reports/learners/lareport_learners.php
@@ -142,7 +142,7 @@ class lareport_learners extends report_base {
         $headingtable = get_string('most_active_learners', 'lareport_learners');
 
         return array_merge(
-            helper::generateCourseParticipationList($courseid, 5)
+            helper::generateCourseParticipationList($courseid, 10)
             // $this->languagesandcountries($courseid)
         );
     }
