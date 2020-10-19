52a4f9e41f93efef164e6323d83c35e7c8430956
Thomas Dondorf <dondorf@lfi.rwth-aachen.de>
2020-10-19 10:47:59 +0200

    Version increased and logstore dependency updated

--- version.php
+++ version.php
@@ -26,12 +26,12 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_learning_analytics';
 
-$plugin->version = 2020101302;
+$plugin->version = 2020101900;
 $plugin->release = 'v0.10.0';
 $plugin->maturity = MATURITY_BETA;
 
 $plugin->requires = 2017111302;
 
 $plugin->dependencies = [
-    'logstore_lanalytics' => 2020051300,
+    'logstore_lanalytics' => 2020101607,
 ];
